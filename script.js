4ba86b5989cb72c3dd6c62c53c69d6ec46cbb14d
dima-dj <155115167+dima-dj@users.noreply.github.com>
2025-02-26 23:38:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,118 +1,129 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const bird = document.querySelector(".bird")
-    const gameDisplay = document.querySelector(".game-container")
-    const ground = document.querySelector(".ground-moving")
-  
-    const birdLeft = 220
-    let birdBottom = 100
-    const gravity = 3
-    let isGameOver = false
-    const gap = 430
-    let score = 0
-  
-    function startGame() {
-      birdBottom -= gravity
-      bird.style.bottom = birdBottom + "px"
-      bird.style.left = birdLeft + "px"
+let move_speed = 3, grativy = 0.5;
+let bird = document.querySelector('.bird');
+let img = document.getElementById('bird-1');
+let sound_point = new Audio('sounds effect/point.mp3');
+let sound_die = new Audio('sounds effect/die.mp3');
+
+// getting bird element properties
+let bird_props = bird.getBoundingClientRect();
+
+// This method returns DOMReact -> top, right, bottom, left, x, y, width and height
+let background = document.querySelector('.background').getBoundingClientRect();
+
+let score_val = document.querySelector('.score_val');
+let message = document.querySelector('.message');
+let score_title = document.querySelector('.score_title');
+
+let game_state = 'Start';
+img.style.display = 'none';
+message.classList.add('messageStyle');
+
+document.addEventListener('keydown', (e) => {
+    
+    if(e.key == 'Enter' && game_state != 'Play'){
+        document.querySelectorAll('.pipe_sprite').forEach((e) => {
+            e.remove();
+        });
+        img.style.display = 'block';
+        bird.style.top = '40vh';
+        game_state = 'Play';
+        message.innerHTML = '';
+        score_title.innerHTML = 'Score : ';
+        score_val.innerHTML = '0';
+        message.classList.remove('messageStyle');
+        play();
     }
-    const gameTimerId = setInterval(startGame, 20)
-  
-    function control(e) {
-      if (e.type === "keyup" && (e.keyCode === 32 || e.keyCode === 38)) {
-        jump()
-      } else if (e.type === "touchstart") {
-        jump()
-        e.preventDefault()
-      }
+});
+
+function play(){
+    function move(){
+        if(game_state != 'Play') return;
+
+        let pipe_sprite = document.querySelectorAll('.pipe_sprite');
+        pipe_sprite.forEach((element) => {
+            let pipe_sprite_props = element.getBoundingClientRect();
+            bird_props = bird.getBoundingClientRect();
+
+            if(pipe_sprite_props.right <= 0){
+                element.remove();
+            }else{
+                if(bird_props.left < pipe_sprite_props.left + pipe_sprite_props.width && bird_props.left + bird_props.width > pipe_sprite_props.left && bird_props.top < pipe_sprite_props.top + pipe_sprite_props.height && bird_props.top + bird_props.height > pipe_sprite_props.top){
+                    game_state = 'End';
+                    message.innerHTML = 'Game Over'.fontcolor('red') + '<br>Press Enter To Restart';
+                    message.classList.add('messageStyle');
+                    img.style.display = 'none';
+                    sound_die.play();
+                    return;
+                }else{
+                    if(pipe_sprite_props.right < bird_props.left && pipe_sprite_props.right + move_speed >= bird_props.left && element.increase_score == '1'){
+                        score_val.innerHTML =+ score_val.innerHTML + 1;
+                        sound_point.play();
+                    }
+                    element.style.left = pipe_sprite_props.left - move_speed + 'px';
+                }
+            }
+        });
+        requestAnimationFrame(move);
     }
-  
-    function jump() {
-      if (birdBottom < 500) birdBottom += 50
-      bird.style.bottom = birdBottom + "px"
-    }
-  
-    document.addEventListener("keyup", control)
-    document.addEventListener("touchstart", control)
-  
-    function generateObstacle() {
-      let obstacleLeft = 500
-      const randomHeight = Math.random() * 60
-      const obstacleBottom = randomHeight
-      const obstacle = document.createElement("div")
-      const topObstacle = document.createElement("div")
-      if (!isGameOver) {
-        obstacle.classList.add("obstacle")
-        topObstacle.classList.add("topObstacle")
-      }
-      gameDisplay.appendChild(obstacle)
-      gameDisplay.appendChild(topObstacle)
-      obstacle.style.left = obstacleLeft + "px"
-      topObstacle.style.left = obstacleLeft + "px"
-      obstacle.style.bottom = obstacleBottom + "px"
-      topObstacle.style.bottom = obstacleBottom + gap + "px"
-  
-      function moveObstacle() {
-        obstacleLeft -= 2
-        obstacle.style.left = obstacleLeft + "px"
-        topObstacle.style.left = obstacleLeft + "px"
-  
-        if (obstacleLeft === -60) {
-          clearInterval(timerId)
-          gameDisplay.removeChild(obstacle)
-          gameDisplay.removeChild(topObstacle)
+    requestAnimationFrame(move);
+
+    let bird_dy = 0;
+    function apply_gravity(){
+        if(game_state != 'Play') return;
+        bird_dy = bird_dy + grativy;
+        document.addEventListener('keydown', (e) => {
+            if(e.key == 'ArrowUp' || e.key == ' '){
+                img.src = 'images/Bird-2.png';
+                bird_dy = -7.6;
+            }
+        });
+
+        document.addEventListener('keyup', (e) => {
+            if(e.key == 'ArrowUp' || e.key == ' '){
+                img.src = 'images/Bird.png';
+            }
+        });
+
+        if(bird_props.top <= 0 || bird_props.bottom >= background.bottom){
+            game_state = 'End';
+            message.style.left = '28vw';
+            window.location.reload();
+            message.classList.remove('messageStyle');
+            return;
         }
-        if (
-          (obstacleLeft > 200 &&
-            obstacleLeft < 280 &&
-            birdLeft === 220 &&
-            (birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap - 200)) ||
-          birdBottom === 0
-        ) {
-          gameOver()
-          clearInterval(timerId)
+        bird.style.top = bird_props.top + bird_dy + 'px';
+        bird_props = bird.getBoundingClientRect();
+        requestAnimationFrame(apply_gravity);
+    }
+    requestAnimationFrame(apply_gravity);
+
+    let pipe_seperation = 0;
+
+    let pipe_gap = 35;
+
+    function create_pipe(){
+        if(game_state != 'Play') return;
+
+        if(pipe_seperation > 115){
+            pipe_seperation = 0;
+
+            let pipe_posi = Math.floor(Math.random() * 43) + 8;
+            let pipe_sprite_inv = document.createElement('div');
+            pipe_sprite_inv.className = 'pipe_sprite';
+            pipe_sprite_inv.style.top = pipe_posi - 70 + 'vh';
+            pipe_sprite_inv.style.left = '100vw';
+
+            document.body.appendChild(pipe_sprite_inv);
+            let pipe_sprite = document.createElement('div');
+            pipe_sprite.className = 'pipe_sprite';
+            pipe_sprite.style.top = pipe_posi + pipe_gap + 'vh';
+            pipe_sprite.style.left = '100vw';
+            pipe_sprite.increase_score = '1';
+
+            document.body.appendChild(pipe_sprite);
         }
-        if (obstacleLeft === 220) {
-          score++
-          updateScore()
-        }
-      }
-      const timerId = setInterval(moveObstacle, 20)
-      if (!isGameOver) setTimeout(generateObstacle, 3000)
+        pipe_seperation++;
+        requestAnimationFrame(create_pipe);
     }
-    generateObstacle()
-  
-    function gameOver() {
-      clearInterval(gameTimerId)
-      isGameOver = true
-      document.removeEventListener("keyup", control)
-      document.removeEventListener("touchstart", control)
-      ground.classList.add("ground")
-      ground.classList.remove("ground-moving")
-      displayGameOver()
-    }
-  
-    function updateScore() {
-      const scoreDisplay = document.querySelector(".score")
-      scoreDisplay.textContent = `Score: ${score}`
-    }
-  
-    function displayGameOver() {
-      const gameOverDisplay = document.createElement("div")
-      gameOverDisplay.classList.add("game-over")
-      gameOverDisplay.innerHTML = `
-              <h1>Game Over!</h1>
-              <p>Your score: ${score}</p>
-              <button class="restart-btn">Restart</button>
-          `
-      gameDisplay.appendChild(gameOverDisplay)
-  
-      const restartBtn = document.querySelector(".restart-btn")
-      restartBtn.addEventListener("click", restartGame)
-    }
-  
-    function restartGame() {
-      window.location.reload()
-    }
-  })
-  
-  
+    requestAnimationFrame(create_pipe);
+}
